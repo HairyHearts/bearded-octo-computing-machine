9e3069207480663ecdaf3a1192f6d0d6eccd1c70
Alessandro Mancini <alem1979@gmail.com>
2014-02-09 10:37:46 +0100

    aggiunta notifica di ricezione... occhio ai TODO

--- MainActivity/src/org/hairyhearts/GCMService.java
+++ MainActivity/src/org/hairyhearts/GCMService.java
@@ -1,13 +1,17 @@
 package org.hairyhearts;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import com.baasbox.android.BaasDocument;
 import com.baasbox.android.BaasException;
-import com.baasbox.android.BaasHandler;
 import com.baasbox.android.BaasResult;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
@@ -34,10 +38,32 @@ public class GCMService extends IntentService {
                 BaasDocument doc;
                 try {
                     doc = res.get();
-
+                                        
                     Log.d(TAG, "from: " + doc.getString("from"));
                     Log.d(TAG, "to: " + doc.getString("to"));
                     Log.d(TAG, "msg: " + doc.getString("msg"));
+
+                    String from = doc.getString("from");
+                    
+                    PendingIntent pendingIntent =
+                            PendingIntent.getActivity(
+                            this,
+                            0,
+                            new Intent(this, SuccessActivity.class), //TODO aggiornare activity per decryptare
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                    
+                    
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.stat_notify_chat) //TODO aggiornare icona
+                        .setContentTitle("New crypted message")
+                        .setContentText(from)                    
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+                    
+                    NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);                   
+                    manager.notify(0, builder.build());                    
+                    
                 } catch (BaasException e) {
                     e.printStackTrace();
                 }
--- MainActivity/src/org/hairyhearts/SuccessActivity.java
+++ MainActivity/src/org/hairyhearts/SuccessActivity.java
@@ -21,53 +21,5 @@ public class SuccessActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_success);
-
-        findViewById(R.id.sendBtn).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMessage();
-            }
-        });
-    }
-
-    private void sendMessage() {
-        String from = BaasUser.current().getName();
-        String to = ((EditText) findViewById(R.id.to)).getText().toString();
-        String msg = ((EditText) findViewById(R.id.message)).getText().toString();
-
-        new SendMessage(from, to, msg).execute();
-    }
-
-    public class SendMessage extends AsyncTask<Void, Void, String> {
-        private String from;
-        private String to;
-        private String msg;
-
-        SendMessage(String from, String to, String msg) {
-            this.from = from;
-            this.to = to;
-            this.msg = msg;
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            BaasDocument doc = new BaasDocument("message");
-            doc.putString("from", from);
-            doc.putString("to", to);
-            doc.putString("msg", msg);
-            doc.saveSync(SaveMode.IGNORE_VERSION);
-
-            return doc.getId();
-        }
-
-        @Override
-        protected void onPostExecute(String id) {
-            BaasUser.withUserName(to).send(new JsonObject().putString("message", id), new BaasHandler<Void>() {
-                @Override
-                public void handle(BaasResult<Void> voidBaasResult) {
-                    Toast.makeText(SuccessActivity.this, "Sent message", Toast.LENGTH_LONG).show();
-                }
-            });
-        }
     }
 }
